4e8706487ae18b1dcf3e195df24aaf1f41dd58a7
Giancarlo Razzolini <grazzolini@gmail.com>
2015-05-04 10:40:54 -0300

    * The profilealert script wasn't sourcing the configuration file, which caused it to not work correctly.

--- chkboot-profilealert.sh
+++ chkboot-profilealert.sh
@@ -7,6 +7,8 @@
 #
 # license: GPLv2
 
+source /etc/default/chkboot.conf
+
 # only try to display chkboot changes if the 'proofile' alert style has been selected
 if [ ! $(echo "${CHKBOOT_STYLES}" | grep -c "profile") = 0 ]; then
     # run whatever issues exist then return the response
