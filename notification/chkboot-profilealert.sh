5e0f52d7a8175cf0d672dcb502ef4786cf58a0fb
Thorben T <r00t@constancy.org>
2019-07-31 01:46:21 +0200

    profilealert.sh: parse defaults-file instead of sourcing,
    as sourcing it in profile.d will pollute the user's environment with all it's settings

--- notification/chkboot-profilealert.sh
+++ notification/chkboot-profilealert.sh
@@ -5,10 +5,8 @@
 #
 # license: GPLv2
 
-. /etc/default/chkboot.conf
-
 # only try to display chkboot changes if the 'proofile' alert style has been selected
-if [ ! $(echo "${CHKBOOT_STYLES}" | grep -c "profile") = 0 ]; then
+if grep "^CHKBOOT_STYLES=.*profile" /etc/default/chkboot.conf >/dev/null ; then
     # run whatever issues exist then return the response
     chkboot-check
     return "$?"
